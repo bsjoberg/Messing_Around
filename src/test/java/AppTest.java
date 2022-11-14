57fb4953ab9f8f3386c3da4302838b984d5292f8
brian <brian.sjoberg@excella.com>
2022-11-14 17:43:33 -0500

    Get junit working.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
+    @Test
+    public void testHello(){
+        Assertions.assertEquals(1, 1);
+    }
 }
